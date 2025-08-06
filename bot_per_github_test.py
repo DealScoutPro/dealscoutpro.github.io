6d86f624822578417a5513de97dc743b81085681
DealScoutPro <alessandro.desogus.96@gmail.com>
2025-08-06 19:10:41 +0200

    Update bot_per_github_test.py

--- bot_per_github_test.py
+++ bot_per_github_test.py
@@ -4,20 +4,19 @@ import re
 import os
 from git import Repo
 
-# Credenziali del bot (caricate da segreti GitHub)
+# Credenziali del bot e dell'app (caricate da segreti GitHub)
 telegram_bot_token = os.environ.get('TELEGRAM_BOT_TOKEN')
 channel_username = os.environ.get('CHANNEL_USERNAME')
-# Questi non sono più utilizzati per l'autenticazione, ma servono comunque
-# per l'inizializzazione del client. Puoi usare valori fittizi ma validi.
-api_id = 2004068213 # Valore fittizio, ma deve essere un numero
-api_hash = 'qualche_stringa' # Valore fittizio, ma deve essere una stringa
+
+# Nuovi segreti da aggiungere su GitHub
+telegram_api_id = os.environ.get('TELEGRAM_API_ID')
+telegram_api_hash = os.environ.get('TELEGRAM_API_HASH')
 
 # Credenziali GitHub (caricate da segreti GitHub)
 github_token = os.environ.get('GH_TOKEN')
 
-# Inizializza il client di Telegram con il token del bot
-# Il secondo parametro è l'api_id e il terzo è l'api_hash
-client = TelegramClient('deal_bot', api_id, api_hash)
+# Inizializza il client di Telegram con l'API ID e l'API Hash
+client = TelegramClient('deal_bot_session', telegram_api_id, telegram_api_hash)
 
 # Funzione per estrarre i dati da un messaggio
 def extract_offer_data(message):
@@ -64,7 +63,7 @@ def extract_offer_data(message):
 async def generate_and_sync_json():
     offers = []
     
-    # Autenticazione esplicita in modalità bot
+    # Autenticazione esplicita in modalità bot con il token
     await client.start(bot_token=telegram_bot_token)
     
     # Recupera il canale
