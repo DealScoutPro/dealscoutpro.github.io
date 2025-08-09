19a66b3b1bd90336ca841c5037c661c8be9525eb
DealScoutPro <alessandro.desogus.96@gmail.com>
2025-08-09 19:36:49 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,21 +9,33 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+  // Gestisce la richiesta per data.json con la strategia "stale-while-revalidate"
+  if (event.request.url.includes('data.json')) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.match(event.request).then(response => {
+          const fetchPromise = fetch(event.request).then(networkResponse => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+          return response || fetchPromise;
+        });
       })
-  );
+    );
+  } else {
+    // Per tutti gli altri file, utilizza la strategia "cache-first"
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  }
 });
